6247a717d2e2aca5ccbb4077578d141051aba7e6
Aleksey Rubanovskiy <zombinome@outlook.com>
2022-02-22 15:42:31 +0100

    Add mock code for external services

---
+++ BitMover.Challenge/BitMover.Challenge.Mocks/AnalyticsApiClient.cs
@@ -0,0 +1,28 @@
+﻿using BitMover.Challenge.Contracts.Analytics;
+
+namespace BitMover.Challenge.Mocks
+{
+    public class AnalyticsApiClient : IAnalyticsApiClient
+    {
+        private const int RowsInResponse = 5;
+
+        public Task<LicenseSessionsResponse> GetLicenseSessionsAsync(string licenceKey, DateTime from, DateTime to)
+        {
+            var random = new Random();
+            var rows = new List<long[]>();
+            for (var i = 0; i < RowsInResponse; i++)
+            {
+                long timestamp = DateTime.UtcNow.Ticks; // There should be UNIX timestamp, but I don't plan to use it, so don't care
+                long sessions = random.Next(0, 1000);
+                rows.Add(new long[] { timestamp, sessions });
+            }
+            var result = new LicenseSessionsResponse()
+            {
+                RowCount = rows.Count,
+                Rows = rows,
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ BitMover.Challenge/BitMover.Challenge.Mocks/BillingMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using BitMover.Challenge.Contracts.Billing;
+using Newtonsoft.Json;
+
+namespace BitMover.Challenge.Mocks
+{
+    public class BillingMessageSender : IBillingMessageSender
+    {
+        public Task SendMessageAsync(BillingMessage message)
+        {
+            var messageStr = JsonConvert.ToString(message);
+            Console.WriteLine("Sent billing message: " + messageStr);
+            return Task.CompletedTask;
+        }
+    }
+}
